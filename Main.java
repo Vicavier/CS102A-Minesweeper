1f404b6e259ab4d40aa25541ecdf9cf89dc93950
Vicavier <91972567+Vicavier@users.noreply.github.com>
2022-03-03 17:52:17 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import Frames.LoginFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame loginFrame=new LoginFrame();
+
+        });
+    }
+}
